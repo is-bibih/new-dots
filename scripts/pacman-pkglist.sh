3b6e32d10fb18df1eee835050c7b6ac93b63ca7f
vivi m <vivianamaldes@gmail.com>
2021-12-26 13:24:12 -0600

    pkglist pacman hook

---
+++ scripts/pacman-pkglist.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+mkdir /etc/pacman.d/hooks
+echo "[Trigger]
+Operation = Install
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+When = PostTransaction
+Exec = /bin/sh -c '/usr/bin/pacman -Qqe > /home/vivi/pkglist.txt'" > /etc/pacman.d/hooks/pkglist.hook
