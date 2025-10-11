b26d020aae6c9501390cb0808dc93b466de1f64f
vivi <vivianamaldes@gmail.com>
2025-10-11 15:04:11 +0200

    bookmark updates for nnn

--- .config/nnn/config.sh
+++ .config/nnn/config.sh
@@ -2,4 +2,4 @@
 
 export NNN_TRASH=1
 export NNN_PLUG='d:dragdrop;i:imgview;c:cppath'
-export NNN_BMS="d:$HOME/Downloads;a:$HOME/anime;g:$HOME/gits;e:$HOME/odrive/ep2;v:$HOME/odrive/vivi"
+export NNN_BMS="d:$HOME/Downloads;g:$HOME/gits;e:$HOME/odrive/ep3;v:$HOME/odrive/vivi"
